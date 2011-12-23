4579416b0afa4514ca4171174960d670c1b2fc75
Nick Cottrell <nick.a.cottrell@gmail.com>
2011-12-23 14:40:43 -0800

    first commit

---
+++ application/forms/Guestbook.php
@@ -0,0 +1,41 @@
+<?php
+
+class Application_Form_Guestbook extends Zend_Form
+{
+
+    public function init()
+    {
+        // Set the method for the display form to POST
+        $this->setMethod('post');
+
+        // Add an email element
+        $this->addElement('text', 'email', array(
+            'label'      => 'Email:',
+            'required'   => false,
+            'filters'    => array('StringTrim'),
+            'validators' => array(
+                'EmailAddress',
+            )
+        ));
+
+        // Add the comment element
+        $this->addElement('textarea', 'comment', array(
+            'label'      => 'Post:',
+            'required'   => true,
+            'validators' => array(
+                array('validator' => 'StringLength', 'options' => array(0, 140))
+                )
+        ));
+
+        // Add the submit button
+        $this->addElement('submit', 'submit', array(
+            'ignore'   => true,
+            'label'    => 'Post Something',
+        ));
+
+        // And finally add some CSRF protection
+        $this->addElement('hash', 'csrf', array(
+            'ignore' => true,
+        ));
+    }
+}
